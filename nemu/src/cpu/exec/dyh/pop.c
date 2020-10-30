04e4914fefa181d3ee61872c1c8b9c70586fd3a8
tracer-ics2016 <tracer@njuics.org>
2020-10-30 14:28:48 +0000

    >  compile NEMU 151220000 simpleedu Linux simpleedu 4.15.0-122-generic #124-Ubuntu SMP Thu Oct 15 13:03:05 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  14:28:48 up  4:04,  1 user,  load average: 0.44, 0.78, 0.76 9ed4ecd24cfeba8935106e982d3c358de3f4208a

--- nemu/src/cpu/exec/dyh/pop.c
+++ nemu/src/cpu/exec/dyh/pop.c
@@ -1,15 +1,15 @@
 #include "cpu/exec/helper.h"
 
 #define DATA_BYTE 1
-#include "cmp-template.h"
+#include "pop-template.h"
 #undef DATA_BYTE
 
 #define DATA_BYTE 2
-#include "cmp-template.h"
+#include "pop-template.h"
 #undef DATA_BYTE
 
 #define DATA_BYTE 4
-#include "cmp-template.h"
+#include "pop-template.h"
 #undef DATA_BYTE
 
 make_helper_v(pop_rm)
